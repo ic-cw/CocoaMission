f09ef5c1b5792fc6205c35d20b1009c9bc452b6b
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-01 15:29:54 +0900

    feat : 구구단을 출력하는 로직 작성

---
+++ src/main/java/MultiplicationTable/MultiplicationTable.java
@@ -0,0 +1,29 @@
+package main.java.MultiplicationTable;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args){
+        inputStartAndEnd();
+    }
+
+    private static void inputStartAndEnd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("출력할 구구단의 첫번째 단을 입력해주세요 : ");
+        int startTable = scanner.nextInt();
+        System.out.print("출력할 구구단의 마지막 단을 입력해주세요 : ");
+        int endTable = scanner.nextInt();
+        printMultiplicationTable(startTable, endTable);
+    }
+
+    private static void printMultiplicationTable(int startTable, int endTable){
+        for (int j = startTable ; j < endTable + 1 ; j++){
+            System.out.println("-------" + startTable + "단----------");
+            for(int i = 1 ; i < 10 ; i++) {
+                System.out.println(startTable + " * " + i + " = " + startTable*i);
+            }
+            startTable++;
+        }
+    }
+}
