4ba25d713b8c6d23bdd0f05c316ade8e6c35ca4b
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-01 14:36:44 +0900

    feat : 구구단 시작과 끝단을 입력받는 로직 작성

--- src/MultiplicationTable/MultiplicationTable.java
+++ src/MultiplicationTable/MultiplicationTable.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(){
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
+        inputStartAndEnd();
+    }
 
+    private static void inputStartAndEnd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("출력할 구구단의 첫번째 단을 입력해주세요 : ");
+        int startTable = scanner.nextInt();
+        System.out.println("출력할 구구단의 마지막 단을 입력해주세요 : ");
+        int endTable = scanner.nextInt();
     }
 }
