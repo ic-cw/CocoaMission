557382dc8f1cbb8db7de9f31a03ba48cd0f55d6e
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-01 16:29:24 +0900

    feat : 알람시계 반복문 연습

---
+++ src/main/java/Day1/PracticeAlarmClock.java
@@ -0,0 +1,29 @@
+package main.java.Day1;
+
+import java.util.Scanner;
+
+public class PracticeAlarmClock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = scanner.nextInt();
+        int minute = scanner.nextInt();
+
+        if (minute >= 45){
+            minute = minute - 45;
+            System.out.println(hour);
+            System.out.println(minute);
+        } else {
+            if (hour == 0){
+                hour = 23;
+                minute = minute + 15;
+                System.out.println(hour);
+                System.out.println(minute);
+            }else{
+                hour -= 1;
+                minute = minute + 15;
+                System.out.println(hour);
+                System.out.println(minute);
+            }
+        }
+    }
+}
