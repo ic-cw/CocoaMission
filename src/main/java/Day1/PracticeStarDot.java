7fd9a76d68afb9d6d2a3a4cf30deb13ce4b29cd4
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-01 16:43:21 +0900

    feat : 별찍기1 연습

---
+++ src/main/java/Day1/PracticeStarDot.java
@@ -0,0 +1,16 @@
+package main.java.Day1;
+
+import java.util.Scanner;
+
+public class PracticeStarDot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int starCount = scanner.nextInt();
+        for (int i = 0 ; i < starCount ; i++){
+            for (int j = 0 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
