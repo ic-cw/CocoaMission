805a5e220c4559ac3ca520898a911c404cde19d5
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-02 10:52:37 +0900

    chore : 클래스명에 _ 사용x 변경

---
+++ src/main/java/Day1/PracticeStarDot2.java
@@ -0,0 +1,16 @@
+package main.java.Day1;
+
+import java.util.Scanner;
+
+public class PracticeStarDot2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int stars = scanner.nextInt();
+
+        for (int i = 0 ; i < stars ; i++){
+            for(int j = stars ; j > 0 ; j--){
+                System.out.println("*");
+            }
+        }
+    }
+}
