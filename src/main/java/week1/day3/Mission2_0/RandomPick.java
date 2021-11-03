33f45cbd9d74111ab62f2011201bfaec514e4f3d
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-03 11:32:05 +0900

    feat : 출력할 멤버수 입력받기

--- src/main/java/week1/day3/Mission2_0/RandomPick.java
+++ src/main/java/week1/day3/Mission2_0/RandomPick.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class RandomPick {
     public static void main(String[] args) {
         List<String> names = generateShuffledNames();
+        int count = inputCount();
     }
 
     private static List<String> generateShuffledNames() {
@@ -13,5 +14,11 @@ public class RandomPick {
         List<String> names = new ArrayList<>(Arrays.asList(name));
         Collections.shuffle(names);
         return names;
+    }
+
+    private static int inputCount() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("몇명인가요?  ");
+        return sc.nextInt();
     }
 }
