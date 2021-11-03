3fc3714182cf789f7d754219b186eb468dec7f3a
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-03 11:18:23 +0900

    chore : 패키지 구성 변경

--- src/main/java/week1/day1/PracticeStarDot1.java
+++ src/main/java/week1/day1/PracticeStarDot1.java
@@ -1,4 +1,4 @@
-package main.java.Day1;
+package main.java.week1.day1;
 
 import java.util.Scanner;
 
---
+++ src/main/java/week1/day1/PracticeSumOfNumber.java
@@ -0,0 +1,13 @@
+package main.java.week1.day1;
+
+import java.util.Scanner;
+
+public class PracticeSumOfNumber {
+    ///undone
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberLength = scanner.nextInt();
+        String numbers = scanner.nextLine();
+        char[] digits = numbers.toCharArray();
+    }
+}
