0eff79738ac558e0cc65b28d041e0ef3a27f0b78
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-04 15:36:13 +0900

    feat : 각자의 좌표값을 상태로 가질 Character, Bomb, Monster클래스 생성

---
+++ src/main/java/week1/day3/mission2/simplerpg/Bomb.java
@@ -0,0 +1,4 @@
+package main.java.week1.day3.mission2.simplerpg;
+
+public class Bomb {
+}
---
+++ src/main/java/week1/day3/mission2/simplerpg/Monster.java
@@ -0,0 +1,12 @@
+package main.java.week1.day3.mission2.simplerpg;
+
+public class Monster {
+    int xCoordinate;
+    int yCoordinate;
+
+    Monster(int xCoordinate, int yCoordinate){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+}
