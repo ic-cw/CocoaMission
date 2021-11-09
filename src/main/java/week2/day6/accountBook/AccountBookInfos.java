703aaff02dd0f54d29cc45705835a6d7ef85536c
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-10 01:13:31 +0900

    feat : 가계부 전체를 담을 AccountBookInfos 클래스를 싱글톤 패턴으로 작성

---
+++ src/main/java/week2/day6/accountBook/AccountBookInfos.java
@@ -0,0 +1,35 @@
+package main.java.week2.day6.accountBook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class AccountBookInfos {
+    private static final AccountBookInfos instance = new AccountBookInfos();
+    private Map<Integer, AccountBookInfo> accountBookInfos = new HashMap<>();
+    private int order = 0;
+    private int balance = 0;
+
+    private AccountBookInfos() {
+    }
+
+    public static AccountBookInfos getInstance() {
+        return instance;
+    }
+
+    public void addAccountBookInfo(AccountBookInfo accountBookInfo) {
+        this.accountBookInfos.put(++order, accountBookInfo);
+        setBalance(accountBookInfo.getIncome(), accountBookInfo.getExpenditure());
+    }
+
+    public void deleteAccountBookInfo(int deleteOrder) {
+        accountBookInfos.remove(deleteOrder);
+    }
+
+    private void setBalance(int income, int expenditure) {
+        this.balance += income;
+        this.balance -= expenditure;
+    }
+}
