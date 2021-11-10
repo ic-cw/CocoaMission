8b575458a4e36eff2c22be6bc07d2dfe09faf58c
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-10 15:29:03 +0900

    feat : 가계부를 조회하는 selectAccountBookInfo 메서드 작성

--- src/main/java/week2/day6/accountBook/AccountBookInfos.java
+++ src/main/java/week2/day6/accountBook/AccountBookInfos.java
@@ -30,10 +30,30 @@ public class AccountBookInfos {
 
     public void updateAccountBookInfo(int updateOrder, AccountBookInfo updatedAccountBookInfo){
         accountBookInfos.replace(updateOrder, updatedAccountBookInfo);
+        //todo balance관련 로직 작성 필요
     }
 
     private void setBalance(int income, int expenditure) {
         this.balance += income;
         this.balance -= expenditure;
+    }
+
+    public void selectAccountBookInfo(String selectedMonth){
+        System.out.println("==순번=======날짜=======내용=======수입=======지출=======");
+        if (selectedMonth.equals("0")){
+            for (int key :accountBookInfos.keySet()){
+                AccountBookInfo accountBookInfo = accountBookInfos.get(key);
+                System.out.println(key + "\t" + accountBookInfo.getDate() + "\t" + accountBookInfo.getContent()
+                        + "\t" + accountBookInfo.getIncome() + "\t" + accountBookInfo.getExpenditure());
+            }
+        }
+        for (int key :accountBookInfos.keySet()){
+            if (accountBookInfos.get(key).getDate().substring(0,6).equals(selectedMonth)) {
+                AccountBookInfo accountBookInfoByMonth = accountBookInfos.get(key);
+                System.out.println(key + "\t" + accountBookInfoByMonth.getDate() + "\t" + accountBookInfoByMonth.getContent()
+                        + "\t" + accountBookInfoByMonth.getIncome() + "\t" + accountBookInfoByMonth.getExpenditure());
+            };
+            //todo 선택한 월이 없을때의 예외처리
+        }
     }
 }
